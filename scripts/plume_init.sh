d4d0d35cc28ed8b7231d55e3c1cb437aaee62e41
Hamza Ben Zekri <hbenzekri@libertyglobal.com>
2020-11-11 12:58:35 +0100

    Add plume agent init script ( RDKB-4761 )
    Add an init script for the plume agent that runs the plume manager if
    SONAdminStatus and SONOperationalStatus are both true.
    
    avoid running the opensync agent before WiFi is initialized ( RDKB-5029 )
    
    If the opensync agent is run before WiFi is initialized, the gateway
    will stay offline on the Plume NOC.

---
+++ scripts/plume_init.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+if [ ! -f /tmp/wifi_first_init ]; then
+    exit 1
+fi
+
+while [ ! -f /tmp/meshagent_initialized ]
+do
+    /bin/sleep 5
+done
+
+start_plume() {
+
+    son_admin="`rpcclient 192.168.254.253 'syscfg get son_admin_status' | tail -n 2 | head -n 1`"
+
+    if [ "$son_admin" != "1" ]; then
+        echo "Plume disabled"
+        exit 1
+    fi
+
+    son_operation="`rpcclient 192.168.254.253 'syscfg get son_operational_status' | tail -n 2 | head -n 1`"
+
+    if [ "$son_operation" != "1" ]; then
+        echo "Plume disabled"
+        exit 1
+    fi
+
+    echo "Starting Plume manager"
+
+    /usr/plume/scripts/managers.init start
+}
+
+stop_plume() {
+    /usr/plume/scripts/managers.init stop
+}
+
+case $1 in
+    start)
+        start_plume
+        ;;
+    stop)
+        stop_plume
+        ;;
+    restart)
+        stop_plume
+        start_plume
+        ;;
+    *)
+        echo "Usage: $0 {start|stop|restart}"
+        ;;
+esac
