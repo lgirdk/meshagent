4985bed7f733c5cc0db9cd67ff3c788b6141cdc5
Hamza Ben Zekri <hbenzekri@libertyglobal.com>
2020-11-11 12:58:35 +0100

    Add plume agent init script ( RDKB-4761 )
    Add an init script for the plume agent that runs the plume manager if
    SONAdminStatus and SONOperationalStatus are both true.
    
    avoid running the opensync agent before WiFi is initialized ( RDKB-5029 )
    
    If the opensync agent is run before WiFi is initialized, the gateway
    will stay offline on the Plume NOC.
    
    plume_init.sh: retry failed RPC commands ( OFW-2009 )
    
    Fix issue causing opensync not to start during bootup
    
    Sometimes during bootup, when the ARM CPU is busy the rpcclient
    commands used to get the opensync syscfg variables fail, causing
    opensync to not be started.
    
    Fix: Retry the rpcclient commands when they fail.
    
    don't start opensync in bridge mode ( OFW-2229 CH7465SI-133 )
    
    [Issue]
    Opensync is started irrespective of device mode.
    
    [Solution]
    Avoid the start of opensync in bridge mode.
    
    [Testing Performed]
    Verify opensync is not started in bride mode even though SON data model is enabled.
    
    Signed-off-by: Sidda(sihiregoudar@libertyglobal.com)
    
    wait for mesh agent in case of plume startup not for during plume stop execution (part of OFW-2336 WiFi factory reset doesn't work from UI/Reset button)
    
    [Description]
    
    handling corner case of loading factory defaults during bootup and calling plume stop, script is getting into the while loop of mesh agent check.
    
    Signed-off-by: BijeeshaVS <bvattakaithayilsudha.contractor@libertyglobal.com>

---
+++ scripts/plume_init.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+
+rpc_error_grep() {
+        grep -q -e "\*\*\* RPC CONNECTED \*\*\*" -e "RPC CONNECTION FAILED \!\!\!\!\!"
+}
+
+if [ ! -f /tmp/wifi_first_init ]; then
+    exit 1
+fi
+
+start_plume() {
+
+    while [ ! -f /tmp/meshagent_initialized ]
+    do
+       /bin/sleep 5
+    done
+
+    son_admin="`rpcclient2 'syscfg get son_admin_status' | tail -n 2 | head -n 1`"
+    while rpc_error_grep <<< $son_admin; do
+        echo "Error getting son_admin_status, retrying"
+        sleep 1
+        son_admin="`rpcclient2 'syscfg get son_admin_status' | tail -n 2 | head -n 1`"
+    done
+
+    if [ "$son_admin" != "1" ]; then
+        echo "Plume son_admin_status disabled"
+        exit 1
+    fi
+
+    son_operation="`rpcclient2 'syscfg get son_operational_status' | tail -n 2 | head -n 1`"
+    while rpc_error_grep <<< $son_operation; do
+        echo "Error getting son_operational_status, retrying"
+        sleep 1
+        son_operation="`rpcclient2 'syscfg get son_operational_status' | tail -n 2 | head -n 1`"
+    done
+
+    if [ "$son_operation" != "1" ]; then
+        echo "Plume son_operational_status disabled"
+        exit 1
+    fi
+
+    br_mode="`rpcclient2 'syscfg get bridge_mode' | tail -n 2 | head -n 1`"
+    if [ "$br_mode" != "0" ]; then
+        echo "In Bridge mode, opensync disabled"
+        exit 1
+    fi
+
+    echo "Starting Plume manager"
+
+    /usr/plume/scripts/managers.init start
+}
+
+stop_plume() {
+    /usr/plume/scripts/managers.init stop
+}
+
+case $1 in
+    start)
+        start_plume
+        ;;
+    stop)
+        stop_plume
+        ;;
+    restart)
+        stop_plume
+        start_plume
+        ;;
+    *)
+        echo "Usage: $0 {start|stop|restart}"
+        ;;
+esac
