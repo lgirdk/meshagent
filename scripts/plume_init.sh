367f5dc18426cc5068fcc4faa83934d154d64368
mkamat <mkamat@libertyglobal.com>
2021-04-23 14:59:07 -0700

    replace rpcclient with rpcclient2

--- scripts/plume_init.sh
+++ scripts/plume_init.sh
@@ -11,14 +11,14 @@ done
 
 start_plume() {
 
-    son_admin="`rpcclient 192.168.254.253 'syscfg get son_admin_status' | tail -n 2 | head -n 1`"
+    son_admin="`rpcclient2 'syscfg get son_admin_status' | tail -n 2 | head -n 1`"
 
     if [ "$son_admin" != "1" ]; then
         echo "Plume disabled"
         exit 1
     fi
 
-    son_operation="`rpcclient 192.168.254.253 'syscfg get son_operational_status' | tail -n 2 | head -n 1`"
+    son_operation="`rpcclient2 'syscfg get son_operational_status' | tail -n 2 | head -n 1`"
 
     if [ "$son_operation" != "1" ]; then
         echo "Plume disabled"
