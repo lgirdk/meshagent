45c09656bf3e5400d48dcc25693d4b2f3e921c87
Hamza Ben Zekri <hbenzekri@libertyglobal.com>
2021-11-10 09:55:13 +0100

    plume_init.sh: retry failed RPC commands ( OFW-2009 )
    Fix issue causing opensync not to start during bootup
    
    Sometimes during bootup, when the ARM CPU is busy the rpcclient
    commands used to get the opensync syscfg variables fail, causing
    opensync to not be started.
    
    Fix: Retry the rpcclient commands when they fail.

--- scripts/plume_init.sh
+++ scripts/plume_init.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+rpc_error_grep() {
+        grep -q -e "\*\*\* RPC CONNECTED \*\*\*" -e "RPC CONNECTION FAILED \!\!\!\!\!"
+}
+
 if [ ! -f /tmp/wifi_first_init ]; then
     exit 1
 fi
@@ -12,16 +16,26 @@ done
 start_plume() {
 
     son_admin="`rpcclient2 'syscfg get son_admin_status' | tail -n 2 | head -n 1`"
+    while rpc_error_grep <<< $son_admin; do
+        echo "Error getting son_admin_status, retrying"
+        sleep 1
+        son_admin="`rpcclient2 'syscfg get son_admin_status' | tail -n 2 | head -n 1`"
+    done
 
     if [ "$son_admin" != "1" ]; then
-        echo "Plume disabled"
+        echo "Plume son_admin_status disabled"
         exit 1
     fi
 
     son_operation="`rpcclient2 'syscfg get son_operational_status' | tail -n 2 | head -n 1`"
+    while rpc_error_grep <<< $son_operation; do
+        echo "Error getting son_operational_status, retrying"
+        sleep 1
+        son_operation="`rpcclient2 'syscfg get son_operational_status' | tail -n 2 | head -n 1`"
+    done
 
     if [ "$son_operation" != "1" ]; then
-        echo "Plume disabled"
+        echo "Plume son_operational_status disabled"
         exit 1
     fi
 
