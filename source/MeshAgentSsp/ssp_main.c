ce09aa721fbd332a53a462fd59b904f6bf367cbb
Andre McCurdy <armccurdy@gmail.com>
2022-01-28 22:01:43 -0800

    drop obsolete support for Windows
      for file in $(find . -type f -name '*.[ch]') ; do unifdef -D_ANSC_LINUX -U_ANSC_WINDOWSNT -U_ANSC_WINDOWS9X -UWIN32 -o $file $file ; done
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/MeshAgentSsp/ssp_main.c
+++ source/MeshAgentSsp/ssp_main.c
@@ -74,7 +74,6 @@ int  cmd_dispatch(int  command)
     {
         case    'e' :
 
-#ifdef _ANSC_LINUX
             MeshInfo("Connect to bus daemon...\n");
 
             {
@@ -94,7 +93,6 @@ int  cmd_dispatch(int  command)
                         				 CCSP_MSG_BUS_CFG,
 				                         gpMeshAgentCfg->DbusPath);
             }
-#endif
             ssp_create(gpMeshAgentCfg);
             ssp_engage(gpMeshAgentCfg);
             break;
@@ -207,7 +205,6 @@ int msgBusInit(const char *pComponentName)
 /*                             Internal functions                             */
 /*----------------------------------------------------------------------------*/
 
-#if defined(_ANSC_LINUX)
 
 /**
  * @brief daemonize is a continous loop running in the background waiting to cater component requests.
@@ -252,7 +249,6 @@ static void daemonize(void) {
 	}
 #endif
 }
-#endif
 
 int CheckAndGetDevicePropertiesEntry( char *pOutput, int size, char *sDevicePropContent )
 {
