756d52728764b3b3140ae975de3c68a45b9f2698
Andre McCurdy <armccurdy@gmail.com>
2022-05-26 15:39:45 -0700

    disable checks for Comcast specific targets XB3, XF3 and HUB4
    These box types are Comcast specific and will not be enabled in
    generic builds. Disabling the code which sets the isPaceXF3 etc
    static flags should allow the compiler to optimise out the unused
    code.
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/MeshAgentSsp/cosa_mesh_apis.c
+++ source/MeshAgentSsp/cosa_mesh_apis.c
@@ -1850,6 +1850,10 @@ static BOOL is_SSID_enabled(void)
 
 static void is_xf3_xb3_platform(void)
 {
+#if 0
+
+    /* These platforms are Comcast specific. There's no point checking for them in generic or non-Comcast builds. */
+
     FILE *cmd;
     char platform[32] = {'\0'};
 
@@ -1870,6 +1874,8 @@ static void is_xf3_xb3_platform(void)
     }
     MeshInfo("platform check XF3:%d, XB3:%d HUB4:%d\n",
                     isPaceXF3, isXB3Platform, isSkyHUB4);
+
+#endif
 }
 
 static BOOL radio_check(void)
